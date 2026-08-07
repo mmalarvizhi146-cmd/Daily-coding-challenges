2107fbfdc9506c7cfe0acc56ad3868d328c4bdfc
Swarthika. M <mmalarvizhi146@gmail.com>
2026-08-07 20:25:57 +0530

    Implement perfect number checker in perfectnum.java
    Added functionality to check if a number is perfect.

--- day 7/perfectnum.java
+++ day 7/perfectnum.java
@@ -1,1 +1,24 @@
+import java.util.Scanner;
+public class perfectnum{
+    public static void main(String[]args){
+        Scanner sc =new Scanner(System.in);
+        System.out.print("Enter value of n:");
+        int n= sc.nextInt();
+        int number=n;
+        int sum=0;
+        for(int i=1;i<n;i++){
+            if(number%i==0)
+                sum=sum+i;
+        }
+        if( sum==number){
+            System.out.print("it is a perfect number");
+        }    
+        else
+        {
+            System.out.print("it isn't a perfect number");
+        }
+    }
+
+    }
+
 
